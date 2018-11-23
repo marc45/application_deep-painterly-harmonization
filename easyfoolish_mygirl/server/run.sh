bb181314b45aa39841c8e8e868f05ca276da98c2
gjpicker <724188300@qq.com>
2018-11-23 16:29:16 +0800

    issues/1

--- easyfoolish_mygirl/server/run.sh
+++ easyfoolish_mygirl/server/run.sh
@@ -13,6 +13,13 @@ export PATH="/$HOME/anaconda2/envs/caffe2_conda/bin:$PATH"
 python -c 'from caffe2.python import core' 2>/dev/null && echo "Success" || echo "Failure"
 
 
+
+export PYTHONPATH=$PYTHONPATH:$(pwd)"/../../"
+python -c 'from easyfoolish_mygirl.common import caffe2_pb2' 2>/dev/null && echo "easyfoolish_mygirl Success" || echo "easyfoolish_mygirl Failure"
+
+
+
+
 cd deep_painterly 
 CUDA_VISIBLE_DEVICES=0 nohup  python daemon_gram.py >a1.log&
 CUDA_VISIBLE_DEVICES=0 nohup  python daemon_paint.py >a2.log&
